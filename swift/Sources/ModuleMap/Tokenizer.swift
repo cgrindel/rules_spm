27252849a61f202255d376f1aedac07f4439f852
Chuck Grindel <chuck@chuckgrindel.com>
2021-07-16 13:09:23 -0600

    Started adding ModuleMap module.

---
+++ swift/Sources/ModuleMap/Tokenizer.swift
@@ -0,0 +1,13 @@
+public class Tokenizer {
+  let input: String
+  var index = 0
+
+  public init(input: String) {
+    self.input = input
+  }
+
+  public func nextToken() -> Token? {
+    // TODO: IMPLEMENT ME!
+    return nil
+  }
+}
