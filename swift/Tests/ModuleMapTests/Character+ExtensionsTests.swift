99a502ed9d77920e006e4dbcc0f91899bdf8c699
Chuck Grindel <chuck@chuckgrindel.com>
2021-07-19 11:02:58 -0600

    Added swiftlint and swiftformat stuff.

--- swift/Tests/ModuleMapTests/Character+ExtensionsTests.swift
+++ swift/Tests/ModuleMapTests/Character+ExtensionsTests.swift
@@ -1,9 +1,20 @@
+@testable import ModuleMap
 import Truth
 import XCTest
-@testable import ModuleMap
 
 class CharacterExtensionsTests: XCTestCase {
-  func testSomething() {
-    fail("IMPLEMENT ME!")
+  func test_isIn() throws {
+    do_isIn_test(char: "1", charSet: .decimalDigits, expected: true)
+    do_isIn_test(char: "a", charSet: .decimalDigits, expected: true)
+  }
+
+  func do_isIn_test(
+    char: Character,
+    charSet: CharacterSet,
+    expected: Bool,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    assertThat(char.isIn(charSet)).isEqualTo(expected, file: file, line: line)
   }
 }
