f63816fb28d659f5840daf2723ea672bbd097144
Chuck Grindel <chuck@chuckgrindel.com>
2021-07-21 12:40:30 -0600

    Added test placeholders.

--- swift/Tests/ModuleMapTests/ParserTests.swift
+++ swift/Tests/ModuleMapTests/ParserTests.swift
@@ -3,7 +3,19 @@ import Truth
 import XCTest
 
 class ParserTests: XCTestCase {
-  func testSomething() {
+  func test_parse_ForExternModule_Success() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForExternModule_MissingModuleToken_Failure() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForExternModule_MissingModuleIDToken_Failure() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForExternModule_MissingPathToken_Failure() throws {
     fail("IMPLEMENT ME!")
   }
 }
