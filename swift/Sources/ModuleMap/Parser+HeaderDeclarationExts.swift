d382a1a9452916a2da8920097ba994f1f5c1a757
Chuck Grindel <chuck@chuckgrindel.com>
2021-07-23 15:09:11 -0600

    Save work.

--- swift/Sources/ModuleMap/Parser+HeaderDeclarationExts.swift
+++ swift/Sources/ModuleMap/Parser+HeaderDeclarationExts.swift
@@ -1,3 +1,5 @@
+// MARK: - Header Declaration Parsing
+
 extension Parser {
   /// Syntax
   ///
@@ -22,16 +24,6 @@ extension Parser {
     var decl = HeaderDeclaration()
 
     // Parse the prefix tokens to determine the type of declaration
-
-    // for prefixToken in prefixTokens {
-    //   switch prefixToken {
-    //   case .reserved(.umbrella):
-    //     decl.type = .umbrella
-    //   case .reserved(.exclude):
-    //     decl.type = .exclude
-    //   case
-    //   }
-    // }
     if let prefixToken = prefixTokens.first {
       switch prefixToken {
       case .reserved(.umbrella):
@@ -43,6 +35,70 @@ extension Parser {
       }
     }
 
+    let pathToken =
+      try nextToken("Collecting the path for header declaration in \(moduleID) module.")
+    guard case let .stringLiteral(path) = pathToken else {
+      throw ParserError.unexpectedToken(
+        pathToken,
+        "Collecting the path for header declaration in \(moduleID) module."
+      )
+    }
+    decl.path = path
+
+    var continueProcessing = true
+    var expectCurlyBracketClose = false
+    while continueProcessing {
+      let token =
+        try nextToken("Collecting end of header declaration for \(path) in \(moduleID) module.")
+      switch token {
+      case .newLine:
+        continueProcessing = false
+      case .curlyBracketOpen:
+        expectCurlyBracketClose = true
+        try parseHeaderDeclarationAttribute(&decl)
+      case .curlyBracketClose:
+        guard expectCurlyBracketClose else {
+          throw ParserError.unexpectedToken(
+            token,
+            "Collecting header attributes for \(path) in \(moduleID) module."
+          )
+        }
+        continueProcessing = false
+      default:
+        throw ParserError.unexpectedToken(
+          token,
+          "Collecting end of header declaration for \(path) in \(moduleID) module."
+        )
+      }
+    }
+    // TODO: Add the rest of the header declaration.
+
     return decl
+  }
+
+  mutating func parseHeaderDeclarationAttribute(_: inout HeaderDeclaration) throws {
+    // TODO: IMPLEMENT ME!
+  }
+}
+
+// MARK: - Single Header Qualifiers Parsing
+
+extension HeaderDeclaration.SingleQualifiers {
+  static func from(moduleID: String, tokens: [Token]) throws -> Self {
+    var qualifiers = Self()
+    for token in tokens {
+      switch token {
+      case .reserved(.private):
+        qualifiers.privateHeader = true
+      case .reserved(.textual):
+        qualifiers.textual = true
+      default:
+        throw ParserError.unexpectedToken(
+          token,
+          "Collecting qualifiers for single header declaration in \(moduleID) module."
+        )
+      }
+    }
+    return qualifiers
   }
 }
--- swift/Tests/ModuleMapTests/Parser+HeaderDeclarationExtsTests.swift
+++ swift/Tests/ModuleMapTests/Parser+HeaderDeclarationExtsTests.swift
@@ -3,7 +3,48 @@ import Truth
 import XCTest
 
 class ParserHeaderDeclarationExtsTests: XCTestCase {
-  func testSomething() {
+  func test_parse_ForModule_WithSingleHeaderDeclNoQualifiers_Success() throws {
+    let text = """
+    module MyModule {
+        header "path/to/header.h"
+    }
+    """
+    let result = try Parser.parse(text)
+    assertThat(result)
+      .hasCount(1)
+      .firstItem {
+        $0.isA(ModuleDeclaration.self) {
+          $0.key(\.moduleID) { $0.isEqualTo("MyModule") }
+            .key(\.members) {
+              $0.hasCount(1)
+                .firstItem {
+                  $0.isA(HeaderDeclaration.self) { $0.isEqualTo(.with {
+                    $0.type = .single(.init())
+                    $0.path = "path/to/header.h"
+                  }) }
+                }
+            }
+        }
+      }
+  }
+
+  func test_parse_ForModule_WithSingleHeaderDeclNoQualifiersWithAttribs_Success() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForModule_WithSingleHeaderDeclAsPrivate_Success() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForModule_WithSingleHeaderDeclAsTextual_Success() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForModule_WithUmbrellaHeader_Success() throws {
+    fail("IMPLEMENT ME!")
+  }
+
+  func test_parse_ForModule_WithExcludeHeader_Success() throws {
     fail("IMPLEMENT ME!")
   }
 }
