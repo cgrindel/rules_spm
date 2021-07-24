418e755686705435fb62acfa29d91c9632aa67c6
Chuck Grindel <chuck@chuckgrindel.com>
2021-07-24 14:04:31 -0600

    Save work.

--- swift/Tests/ModuleMapTests/Parser+HeaderDeclarationExtsTests.swift
+++ swift/Tests/ModuleMapTests/Parser+HeaderDeclarationExtsTests.swift
@@ -35,28 +35,6 @@ class ParserHeaderDeclarationExtsTests: XCTestCase {
         $0.path = "path/to/header.h"
       })
     }
-    // let text = """
-    // module MyModule {
-    //     header "path/to/header.h"
-    // }
-    // """
-    // let result = try Parser.parse(text)
-    // assertThat(result)
-    //   .hasCount(1)
-    //   .firstItem {
-    //     $0.isA(ModuleDeclaration.self) {
-    //       $0.key(\.moduleID) { $0.isEqualTo("MyModule") }
-    //         .key(\.members) {
-    //           $0.hasCount(1)
-    //             .firstItem {
-    //               $0.isA(HeaderDeclaration.self) { $0.isEqualTo(.with {
-    //                 $0.type = .single(.init())
-    //                 $0.path = "path/to/header.h"
-    //               }) }
-    //             }
-    //         }
-    //     }
-    //   }
   }
 
   func test_parse_ForModule_WithSingleHeaderDeclNoQualifiersWithAttribs_Success() throws {
@@ -72,30 +50,6 @@ class ParserHeaderDeclarationExtsTests: XCTestCase {
         $0.mtime = 5678
       })
     }
-    // let text = """
-    // module MyModule {
-    //     header "path/to/header.h" { size 1234 mtime 5678 }
-    // }
-    // """
-    // let result = try Parser.parse(text)
-    // assertThat(result)
-    //   .hasCount(1)
-    //   .firstItem {
-    //     $0.isA(ModuleDeclaration.self) {
-    //       $0.key(\.moduleID) { $0.isEqualTo("MyModule") }
-    //         .key(\.members) {
-    //           $0.hasCount(1)
-    //             .firstItem {
-    //               $0.isA(HeaderDeclaration.self) { $0.isEqualTo(.with {
-    //                 $0.type = .single(.init())
-    //                 $0.path = "path/to/header.h"
-    //                 $0.size = 1234
-    //                 $0.mtime = 5678
-    //               }) }
-    //             }
-    //         }
-    //     }
-    //   }
   }
 
   func test_parse_ForModule_WithSingleHeaderDeclAsPrivate_Success() throws {
@@ -109,54 +63,19 @@ class ParserHeaderDeclarationExtsTests: XCTestCase {
         $0.path = "path/to/header.h"
       })
     }
-    // let text = """
-    // module MyModule {
-    //     private header "path/to/header.h"
-    // }
-    // """
-    // let result = try Parser.parse(text)
-    // assertThat(result)
-    //   .hasCount(1)
-    //   .firstItem {
-    //     $0.isA(ModuleDeclaration.self) {
-    //       $0.key(\.moduleID) { $0.isEqualTo("MyModule") }
-    //         .key(\.members) {
-    //           $0.hasCount(1)
-    //             .firstItem {
-    //               $0.isA(HeaderDeclaration.self) { $0.isEqualTo(.with {
-    //                 $0.type = .single(.with { $0.privateHeader = true })
-    //                 $0.path = "path/to/header.h"
-    //               }) }
-    //             }
-    //         }
-    //     }
-    //   }
   }
 
   func test_parse_ForModule_WithSingleHeaderDeclAsTextual_Success() throws {
-    fail("IMPLEMENT ME!")
-    // let text = """
-    // module MyModule {
-    //     textual header "path/to/header.h"
-    // }
-    // """
-    // let result = try Parser.parse(text)
-    // assertThat(result)
-    //   .hasCount(1)
-    //   .firstItem {
-    //     $0.isA(ModuleDeclaration.self) {
-    //       $0.key(\.moduleID) { $0.isEqualTo("MyModule") }
-    //         .key(\.members) {
-    //           $0.hasCount(1)
-    //             .firstItem {
-    //               $0.isA(HeaderDeclaration.self) { $0.isEqualTo(.with {
-    //                 $0.type = .single(.with { $0.privateHeader = true })
-    //                 $0.path = "path/to/header.h"
-    //               }) }
-    //             }
-    //         }
-    //     }
-    //   }
+    try do_parse_ForModule_Header_test("""
+    module MyModule {
+        textual header "path/to/header.h"
+    }
+    """) {
+      $0.isEqualTo(.with {
+        $0.type = .single(.with { $0.textual = true })
+        $0.path = "path/to/header.h"
+      })
+    }
   }
 
   func test_parse_ForModule_WithUmbrellaHeader_Success() throws {
