afe4ccaefd2b1cbb1ea9d927383c1febcefcf7e8
Chuck Grindel <chuck@chuckgrindel.com>
2021-08-02 16:20:24 -0600

    Added example that attempts to run AsyncKit example. Fails because the dependency module.modulemap files still point to sandboxed header files.

---
+++ examples/package_with_deps/main.swift
@@ -0,0 +1,20 @@
+// AsyncKit imports Logging and NIO for us.
+import AsyncKit
+// import Logging
+// import NIO
+
+let logger = Logger(label: "com.example.main")
+
+let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+defer {
+  try! group.syncShutdownGracefully()
+}
+
+let eventLoop = group.next()
+
+// Use an AsyncKit extension
+let future = eventLoop.future()
+
+try future.wait()
+
+logger.info("Hello World!")
