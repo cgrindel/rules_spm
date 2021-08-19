48192926e40cb330610d41af3d4b218360aa00c0
Chuck Grindel <chuck@chuckgrindel.com>
2021-08-19 14:19:00 -0600

    Route test works, but database save does not.

--- examples/vapor/Sources/App/Configuration/routes.swift
+++ examples/vapor/Sources/App/Configuration/routes.swift
@@ -1,11 +1,31 @@
+import App_Models
 import Vapor
 
+enum AppError: Error {
+    case missingName
+}
+
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    app.get { _ in
+        "It works!"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw AppError.missingName
+        }
+
+        let foo = Foo()
+        foo.name = name
+        // do {
+        //     try foo.save(on: app.db).wait()
+        // } catch {
+        //     // DEBUG BEGIN
+        //     Swift.print("*** CHUCK  error: \(String(reflecting: error))")
+        //     // DEBUG END
+        //     throw error
+        // }
+
+        return "Hello, \(name)!"
     }
 }
