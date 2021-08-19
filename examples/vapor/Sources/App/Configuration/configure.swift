d22f698895fc6055160759a7aac247feb03304fa
Chuck Grindel <chuck@chuckgrindel.com>
2021-08-19 12:57:25 -0600

    Save work.

--- examples/vapor/Sources/App/Configuration/configure.swift
+++ examples/vapor/Sources/App/Configuration/configure.swift
@@ -8,6 +8,9 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(.sqlite(.memory), as: .sqlite)
+    app.migrations.add(CreateFooTable(), to: .sqlite)
+    // Make sure that the database is fully migrated.
+    try app.autoMigrate().wait()
 
     // register routes
     try routes(app)
