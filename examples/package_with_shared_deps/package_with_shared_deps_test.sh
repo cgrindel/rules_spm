73561be5a5467723b03c04444fcc981992424d96
Chuck Grindel <chuck@chuckgrindel.com>
2021-08-27 13:45:39 -0600

    Missed some stuff in the example rename.

--- examples/package_with_shared_deps/package_with_shared_deps_test.sh
+++ examples/package_with_shared_deps/package_with_shared_deps_test.sh
@@ -17,8 +17,8 @@ err_msg() {
   exit 1
 }
 
-workspace="package_with_deps_example"
-binary="$(rlocation "${workspace}/package_with_deps")"
+workspace="package_with_shared_deps_example"
+binary="$(rlocation "${workspace}/package_with_shared_deps")"
 
 expected="Hello World"
 "${binary}" | grep "${expected}" || err_msg "Failed to find expected output. ${expected}"
