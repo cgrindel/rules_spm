1792f58c7441682f99959715d1768a1b74800479
Chuck Grindel <chuck@chuckgrindel.com>
2021-09-16 11:12:21 -0600

    Clean up.

--- spm/internal/exec_spm_build.sh
+++ spm/internal/exec_spm_build.sh
@@ -11,10 +11,6 @@ while (("$#")); do
       worker_exec="${2}"
       shift 2
       ;;
-    # "--build-config")
-    #   build_config="${2}"
-    #   shift 2
-    #   ;;
     "--package-path")
       package_path="${2}"
       shift 2
@@ -57,19 +53,6 @@ for path_dir in "${path_dirs[@]}" ; do
   export PATH="${path_dir}:${PATH}"
 done
 
-# DEBUG BEGIN
-echo >&2 "*** CHUCK:  PATH: ${PATH}" 
-# DEBUG END
-
-# path_dirs=()
-# for spm_utility in "${spm_utilities[@]}" ; do
-#   path_dirs+=("$(dirname "${spm_utility}")")
-# done
-# uniq_path_dirs=$(for d in "${path_dirs[@]}"; do echo "${d}"; done | sort -u)
-# for path_dir in "${uniq_path_dirs[@]}" ; do
-#   export PATH="${path_dir}:${PATH}"
-# done
-
 # The SPM deps that were fetched are in a directory in the source area with the
 # same basename as the build_path.
 fetched_dir="${package_path}/$(basename "${build_path}")"
@@ -79,10 +62,6 @@ fetched_dir="${package_path}/$(basename "${build_path}")"
 # copy the contents of the source directory not the actual directory.
 cp -R -L "${fetched_dir}/." "${build_path}" 
 
-# build_args+=(--manifest-cache none)
-# build_args+=(--disable-repository-cache)
-# build_args+=(--disable-sandbox)
-# build_args+=(--configuration ${build_config})
 build_args+=(--package-path "${package_path}")
 build_args+=(--build-path "${build_path}")
 build_args+=(-Xswiftc "-target" -Xswiftc "${target_triple}")
@@ -94,22 +73,7 @@ if [[ -n "${sdk_name:-}" ]]; then
   build_args+=(-Xswiftc "-sdk" -Xswiftc "${sdk_path}")
 fi
 
-# DEBUG BEGIN
-# echo >&2 "*** CHUCK: swift --version" 
-# "${worker_exec}" swift --version >&2
-# echo >&2 "*** CHUCK: swift package --version" 
-# "${worker_exec}" swift package --version >&2
-# echo >&2 "*** CHUCK: swift build --help" 
-# "${worker_exec}" swift build --help >&2
-# echo >&2 "*** CHUCK:  which git: $(which git)" 
-
-# build_args+=(--verbose)
-# echo >&2 "*** CHUCK:  build_args[@]: ${build_args[@]}" 
-# set -x
-# DEBUG END
-
 # Execute the SPM build
-# "${worker_exec}" build "${build_args[@]}"
 "${worker_exec}" swift build "${build_args[@]}"
 
 # Replace the specified files with the provided ones
