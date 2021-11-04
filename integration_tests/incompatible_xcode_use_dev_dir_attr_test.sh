abada3f6692485ef603096a9c6c096666ecb726f
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-04 14:12:26 -0600

    Save debug stuff.

--- integration_tests/incompatible_xcode_use_dev_dir_attr_test.sh
+++ integration_tests/incompatible_xcode_use_dev_dir_attr_test.sh
@@ -15,10 +15,24 @@ err_msg() {
   exit 1
 }
 
-workspace="cgrindel_rules_spm"
-test_binary="$(rlocation "${workspace}/integration_tests/do_incompatible_xcode_use_dev_dir_attr_test.sh")"
+# DEBUG BEGIN
+echo >&2 "*** CHUCK:  BUILD_WORKING_DIRECTORY: ${BUILD_WORKING_DIRECTORY:-}" 
+echo >&2 "*** CHUCK: orig  pwd: $(pwd)" 
+mkdir fake_home
+export HOME="$(pwd)/fake_home"
+bazel version
+tree
 
-# expected="Hello World"
-# "${binary}" | grep "${expected}" || err_msg "Failed to find expected output. ${expected}"
-. "${test_binary}"
+cd ../../../../../../../..
+echo >&2 "*** CHUCK: workspace  pwd: $(pwd)" 
+ls -l 
+exit 1
+# DEBUG END
+
+# workspace="cgrindel_rules_spm"
+# test_binary="$(rlocation "${workspace}/integration_tests/do_incompatible_xcode_use_dev_dir_attr_test.sh")"
+
+# # expected="Hello World"
+# # "${binary}" | grep "${expected}" || err_msg "Failed to find expected output. ${expected}"
+# . "${test_binary}"
 
