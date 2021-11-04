d5eeb8ea4447b6bdfc2bded41062996dc062184d
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-03 18:01:54 -0600

    Save work.

--- integration_tests/do_bazel_test.sh
+++ integration_tests/do_bazel_test.sh
@@ -2,14 +2,33 @@
 
 set -euo pipefail
 
+normalize_path() {
+  local path="${1}"
+  echo "$(cd "${path}" > /dev/null && pwd)"
+}
+
+get_script_dir() {
+  # echo "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+  echo "$(normalize_path "$(dirname "${BASH_SOURCE[0]}")")"
+}
+
 get_workspace_root() {
-  local script_dir="${1}"
   if [[ ! -z "${BUILD_WORKING_DIRECTORY}"  ]]; then
     echo "${BUILD_WORKING_DIRECTORY}"
   else
+    local script_dir="$(get_script_dir)"
     echo "$(cd "${script_dir}/.." > /dev/null && pwd)"
   fi
 }
+
+# get_workspace_root() {
+#   local script_dir="${1}"
+#   if [[ ! -z "${BUILD_WORKING_DIRECTORY}"  ]]; then
+#     echo "${BUILD_WORKING_DIRECTORY}"
+#   else
+#     echo "$(cd "${script_dir}/.." > /dev/null && pwd)"
+#   fi
+# }
 
 # Switch the default Xcode to be incompatible, then use DEVELOPER_DIR 
 # attribute on spm_repositories to specify the one that should be used.
@@ -34,14 +53,9 @@ while (("$#")); do
 done
 
 starting_dir="$(pwd)"
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
-
-# if [[ ! -z "${BUILD_WORKING_DIRECTORY}"  ]]; then
-#   workspace_root="${BUILD_WORKING_DIRECTORY}"
-# else
-#   workspace_root="$(cd "${script_dir}/.." > /dev/null && pwd)"
-# fi
-workspace_root="$(get_workspace_root "${script_dir}")"
+workspace_root="$(get_workspace_root)"
+# script_dir="$(get_script_dir)"
+# workspace_root="$(get_workspace_root "${script_dir}")"
 
 # Set trap for cleanup
 cleanup() {
