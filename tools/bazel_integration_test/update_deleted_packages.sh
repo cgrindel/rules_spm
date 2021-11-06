b28b4871acec7e616358581a6851f0fc7e04c92f
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-06 13:54:05 -0600

    Added runnable target for update_deleted_packages.

--- tools/bazel_integration_test/update_deleted_packages.sh
+++ tools/bazel_integration_test/update_deleted_packages.sh
@@ -6,11 +6,24 @@
 # For integration tests, we want to be able to glob() up the sources inside a nested package
 # See explanation in .bazelrc
 
-# set -eux
 set -euo pipefail
 
-DIR="$(dirname $0)/../.."
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+
+if [[ -z "${BUILD_WORKING_DIRECTORY}"  ]]; then
+  workspace_root="$(cd "${script_dir}/.." > /dev/null && pwd)"
+else
+  workspace_root="${BUILD_WORKING_DIRECTORY}"
+fi
+
+cd "${workspace_root}"
+
+# Update the .bazelrc file with the deleted packages flag.
 # The sed -i.bak pattern is compatible between macos and linux
 sed -i.bak "/^[^#].*--deleted_packages/s#=.*#=$(\
     find examples/*/* \( -name BUILD -or -name BUILD.bazel \) | xargs -n 1 dirname | paste -sd, -\
-)#" $DIR/.bazelrc && rm .bazelrc.bak
+)#" .bazelrc
+
+# Remove the the backup file.
+rm .bazelrc.bak
+
