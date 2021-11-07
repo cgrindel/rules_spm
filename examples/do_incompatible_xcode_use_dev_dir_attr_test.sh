16865706b191a573e5df5dd4c951c41a640c9fef
Chuck Grindel <chuck@chuckgrindel.com>
2021-11-07 15:30:56 -0700

    Save work.

---
+++ examples/do_incompatible_xcode_use_dev_dir_attr_test.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Switch the default Xcode to be incompatible, then use DEVELOPER_DIR 
+# attribute on spm_repositories to specify the one that should be used.
+
+exit_on_error() {
+  local err_msg="${1:-}"
+  [[ -n "${err_msg}" ]] || err_msg="Unspecified error occurred."
+  echo >&2 "${err_msg}"
+  exit 1
+}
+
+normalize_path() {
+  local path="${1}"
+  if [[ -d "${path}" ]]; then
+    local dirname="$(dirname "${path}")"
+  else
+    local dirname="$(dirname "${path}")"
+    local basename="$(basename "${path}")"
+  fi
+  dirname="$(cd "${dirname}" > /dev/null && pwd)"
+  if [[ -z "${basename:-}" ]]; then
+    echo "${dirname}"
+  fi
+  echo "${dirname}/${basename}"
+}
+
+starting_dir="$(pwd)"
+bazel_cmds=()
+clean=false
+
+# Process args
+while (("$#")); do
+  case "${1}" in
+    "--bazel")
+      bazel_rel_path="${2}"
+      shift 2
+      ;;
+    "--bazel_cmd")
+      bazel_cmds+=("${2}")
+      shift 2
+      ;;
+    "--workspace")
+      workspace_path="${2}"
+      shift 2
+      ;;
+    "--clean")
+      clean=true
+      shift 1
+      ;;
+    *)
+      shift 1
+      ;;
+  esac
+done
+
+
+[[ -n "${bazel_rel_path:-}" ]] || exit_on_error "Must specify the location of the Bazel binary."
+[[ -n "${workspace_path:-}" ]] || exit_on_error "Must specify the location of the workspace file."
+
+starting_path="$(pwd)"
+starting_path="${starting_path%%*( )}"
+bazel="$(normalize_path "${bazel_rel_path}")"
+
+workspace_dir="$(dirname "${workspace_path}")"
+cd "${workspace_dir}"
+
+# BEGIN Custom test logic 
+
+# Find Xcode 12.4
+xcode_12_4_locations=("/Applications/Xcode-12.4.app"  "/Applications/Xcode_12.4.app")
+for path in "${xcode_12_4_locations[@]}" ; do
+  [[ -d "${path}" ]] && xcode_12_4_location="${path}" && break
+done
+[[ -z "${xcode_12_4_location:-}" ]]  && echo >&2 "Could not find Xcode 12.4." && exit 1
+
+# Identify the current default Xcode 
+current_xcode=$(xcode-select --print-path)
+echo "current_xcode: ${current_xcode}"
+
+# Set trap for cleanup
+cleanup() {
+  # sudo xcode-select --switch "${current_xcode}"
+  xcode-select --switch "${current_xcode}"
+  cd "${starting_dir}"
+}
+trap cleanup EXIT
+
+# Switch default Xcode 12.4 which has SPM 5.3.
+# sudo xcode-select --switch "${xcode_12_4_location}"
+xcode-select --switch "${xcode_12_4_location}"
+
+# END Custom test logic 
+
+# Perform a clean first, if specified.
+[[ ${clean} = true ]] && "${bazel}" clean
+
+# Execute the Bazel commands
+for cmd in "${bazel_cmds[@]}" ; do
+  # Break the cmd string into parts
+  read -a cmd_parts <<< ${cmd}
+  # Execute the Bazel command
+  "${bazel}" "${cmd_parts[@]}"
+done
--- examples/incompatible_xcode_use_dev_dir_attr_test.sh
+++ examples/incompatible_xcode_use_dev_dir_attr_test.sh
@@ -2,34 +2,14 @@
 
 set -euo pipefail
 
-exit_on_error() {
-  local err_msg="${1:-}"
-  [[ -n "${err_msg}" ]] || err_msg="Unspecified error occurred."
-  echo >&2 "${err_msg}"
-  exit 1
-}
-
-normalize_path() {
-  local path="${1}"
-  echo "$(cd "${path}" > /dev/null && pwd)"
-}
-
-bazel_cmds=()
+clean=false
 
 # Process args
 while (("$#")); do
   case "${1}" in
-    "--bazel")
-      bazel_rel_path="${2}"
-      shift 2
-      ;;
-    "--bazel_cmd")
-      bazel_cmds+=("${2}")
-      shift 2
-      ;;
-    "--workspace")
-      workspace_path="${2}"
-      shift 2
+    "--clean")
+      clean=true
+      shift 1
       ;;
     *)
       shift 1
@@ -37,45 +17,15 @@ while (("$#")); do
   esac
 done
 
-[[ -n "${bazel_rel_path:-}" ]] || exit_on_error "Must specify the location of the Bazel binary."
-[[ -n "${workspace_path:-}" ]] || exit_on_error "Must specify the location of the workspace file."
+starting_dir="$(pwd)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
-starting_path="$(pwd)"
-starting_path="${starting_path%%*( )}"
-bazel="${starting_path}/${bazel_rel_path}"
+args=()
+args+=(--bazel "$(which bazel)")
+args+=(--workspace "${script_dir}/simple_with_dev_dir/WORKSPACE")
+args+=(--bazel_cmd "info")
+args+=(--bazel_cmd "test //...")
+[[ ${clean} = true ]] && args+=(--clean)
 
-workspace_dir="$(dirname "${workspace_path}")"
-cd "${workspace_dir}"
+. "${script_dir}/do_incompatible_xcode_use_dev_dir_attr_test.sh" "${args[@]}"
 
-# BEGIN Custom test logic 
-
-# Find Xcode 12.4
-xcode_12_4_locations=("/Applications/Xcode-12.4.app"  "/Applications/Xcode_12.4.app")
-for path in "${xcode_12_4_locations[@]}" ; do
-  [[ -d "${path}" ]] && xcode_12_4_location="${path}" && break
-done
-[[ -z "${xcode_12_4_location:-}" ]]  && echo >&2 "Could not find Xcode 12.4." && exit 1
-
-# Identify the current default Xcode 
-current_xcode=$(xcode-select --print-path)
-echo "current_xcode: ${current_xcode}"
-
-# Set trap for cleanup
-cleanup() {
-  sudo xcode-select --switch "${current_xcode}"
-  cd "${starting_dir}"
-}
-trap cleanup EXIT
-
-# Switch default Xcode 12.4 which has SPM 5.3.
-sudo xcode-select --switch "${xcode_12_4_location}"
-
-# END Custom test logic 
-
-# Execute the Bazel commands
-for cmd in "${bazel_cmds[@]}" ; do
-  # Break the cmd string into parts
-  read -a cmd_parts <<< ${cmd}
-  # Execute the Bazel command
-  "${bazel}" "${cmd_parts[@]}"
-done
