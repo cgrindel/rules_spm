2c1f3493e015d4038877c5112708e9c4be656f72
Chuck Grindel <chuck@chuckgrindel.com>
2022-06-17 15:42:37 -0600

    Save work

--- tools/spm_parser/Sources/spm_parser/main.swift
+++ tools/spm_parser/Sources/spm_parser/main.swift
@@ -1,1 +1,20 @@
-print("Hello, world!")
+import ArgumentParser
+import Basics
+import Foundation
+import TSCBasic
+import Workspace
+
+@main
+struct Dump: ParsableCommand {
+    @Argument(
+        help: "The location of the Package.swift file to analyze.",
+        completion: .file()
+    )
+    var packagePath: String
+
+    mutating func run() throws {
+        // DEBUG BEGIN
+        fputs("*** CHUCK packagePath: \(String(reflecting: packagePath))\n", stderr)
+        // DEBUG END
+    }
+}
