be5f7f6c125540957a32f711f20e28875b17deaa
Chuck Grindel <chuck@chuckgrindel.com>
2022-06-18 12:12:10 -0600

    Basic JSON encoding

--- tools/spm_parser/Sources/spm_parser/Dump.swift
+++ tools/spm_parser/Sources/spm_parser/Dump.swift
@@ -6,6 +6,11 @@ import Workspace
 
 @main
 struct Dump: AsyncParsableCommand {
+    enum DumpError: Error {
+        // case failedToEncodeAsJSON
+        case failedToConvertToUTF8
+    }
+
     @Argument(
         help: "The location of the Package.swift file to analyze.",
         completion: .directory,
@@ -14,41 +19,45 @@ struct Dump: AsyncParsableCommand {
     var packagePath: AbsolutePath
 
     // @Option(
-    //     help: "The location to write the "
+    //     help: "The location to write the target descriptions JSON."
     // )
-    // var
+    // var outputPath:
+
+    // let encoder = JSONEncoder()
+    lazy var encoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        return encoder
+    }()
 
     mutating func runAsync() async throws {
-        // DEBUG BEGIN
-        fputs("*** CHUCK packagePath: \(String(reflecting: packagePath))\n", stderr)
-        // DEBUG END
-
         let observability = ObservabilitySystem { print("\($0): \($1)") }
         let workspace = try Workspace(forRootPackage: packagePath)
-
-        // DEBUG BEGIN
-        fputs("*** CHUCK START\n", stderr)
-        // DEBUG END
-
         let manifest = try await workspace.loadRootManifest(
             at: packagePath,
             observabilityScope: observability.topScope
         )
 
-        // DEBUG BEGIN
-        fputs("*** CHUCK manifest: \(String(reflecting: manifest))\n", stderr)
-        // DEBUG END
+        // let products = manifest.products.map { $0.name }.joined(separator: ", ")
+        // print("Products:", products)
 
-        let products = manifest.products.map { $0.name }.joined(separator: ", ")
-        print("Products:", products)
+        // let targets = manifest.targets.map { $0.name }.joined(separator: ", ")
+        // print("Targets:", targets)
+        // // DEBUG BEGIN
+        // fputs("*** CHUCK manifest.targets:\n", stderr)
+        // for (idx, item) in manifest.targets.enumerated() {
+        //     fputs("*** CHUCK   \(idx) : \(String(reflecting: item))\n", stderr)
+        // }
+        // // DEBUG END
 
-        let targets = manifest.targets.map { $0.name }.joined(separator: ", ")
-        print("Targets:", targets)
-        // DEBUG BEGIN
-        fputs("*** CHUCK manifest.targets:\n", stderr)
-        for (idx, item) in manifest.targets.enumerated() {
-            fputs("*** CHUCK   \(idx) : \(String(reflecting: item))\n", stderr)
+        let targets = manifest.targets
+        let jsonData = try encoder.encode(targets)
+        guard let json = String(data: jsonData, encoding: .utf8) else {
+            throw DumpError.failedToConvertToUTF8
         }
+
+        // DEBUG BEGIN
+        fputs("*** CHUCK json: \(json)\n", stderr)
         // DEBUG END
     }
 }
