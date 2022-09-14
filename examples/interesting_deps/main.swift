dac3328b2aee9dd66e1094e6ef90a3ae4b44d1b3
Gabriel Feron <g@leirbag.net>
2022-09-14 16:46:31 +0200

    Add Auth0.swift to interesting deps example

--- examples/interesting_deps/main.swift
+++ examples/interesting_deps/main.swift
@@ -1,8 +1,11 @@
 import libwebp
 import Logging
+import Auth0
 
 let logger = Logger(label: "com.example.main")
 logger.info("Hello World!")
 
 let webpVersion = WebPGetDecoderVersion()
 logger.info("WebP version: \(webpVersion)")
+
+let webAuth = Auth0.webAuth()
